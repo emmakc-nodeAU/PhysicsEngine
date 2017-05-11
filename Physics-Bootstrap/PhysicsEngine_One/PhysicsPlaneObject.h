6c8b74c153376198066f73031042f1c8a7fe4b5f
Emma Cameron <emmakc@gmail.com>
2017-05-12 09:26:55 +1000

    Physics: Shape - Plane: Sets the elements relevant to creating a Plane to add to an object

---
+++ Physics-Bootstrap/PhysicsEngine_One/PhysicsPlaneObject.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "PhysicsShape.h"
+#include <glm\glm.hpp>
+
+class PhysicsPlaneShape : public PhysicsShape
+{
+public:
+	PhysicsPlaneShape();
+	PhysicsPlaneShape(glm::vec2 normal, float distance);
+	~PhysicsPlaneShape();
+
+	virtual void fixedUpdate(glm::vec2 gravity, float timeStep);
+	virtual void debug() {}
+	virtual void makeGizmo();
+	virtual void resetPosition();
+
+	glm::vec2 getNormal() { return m_normal; }
+	float getDistance() { return m_distanceToOrigin; }
+
+protected:
+	glm::vec2 m_normal;
+	float m_distanceToOrigin;
+};
