561b9fbf15d48260a90b3105b8f0bc2d6032e08a
Emma Cameron <emmakc@gmail.com>
2017-05-17 14:08:29 +1000

    Physics: Type class Physics AABB Shape

--- Physics-Bootstrap/PhysicsEngine_One/PhysicsAABBShape.h
+++ Physics-Bootstrap/PhysicsEngine_One/PhysicsAABBShape.h
@@ -6,19 +6,27 @@
 class PhysicsAABBShape : public PhysicsShape
 {
 	PhysicsAABBShape() = delete;
-	PhysicsAABBShape(extents)
+	PhysicsAABBShape(float extents, float width, float height)
 		: PhysicsShape(PhysicsShape::ShapeType::AABB)
 		, m_extents(extents)
+		, m_width (width)
+		, m_height (height)
 	{
 
 	}
 
 	void SetExtents(glm::vec3 extents) { m_extents = extents; }
 	glm::vec3 GetExtents() const { return m_extents; }
+
+	void SetWidth(glm::vec3 width) { m_width = width; }
 	glm::vec3 GetWidth() const { return m_width; }
+
+	void SetHeight(glm::vec3 height) { m_height = height; }
 	glm::vec3 GetHeight() const { return m_height; }
 
 private:
 	glm::vec3 m_extents;
+	glm::vec3 m_width;
+	glm::vec3 m_height;
 	//glm::vec3 pShape;
 };
