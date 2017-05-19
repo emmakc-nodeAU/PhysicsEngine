91a5fb6bbd9714490b1a5297089004aee20d2bb7
Emma Cameron <emmakc@gmail.com>
2017-05-19 16:46:29 +1000

    General Sphere v Sphere formulas added to comments in header

--- Physics-Bootstrap/PhysicsEngine_One/PhysicsSphereShape.h
+++ Physics-Bootstrap/PhysicsEngine_One/PhysicsSphereShape.h
@@ -5,19 +5,17 @@ class PhysicsSphereShape : public PhysicsShape
 {
 public:
 	PhysicsSphereShape() = delete;
+
 	PhysicsSphereShape(float radius)
 		: PhysicsShape(PhysicsShape::ShapeType::Sphere)
 		, m_radius(radius)
 	{
-
 	}
 
 	void SetRadius(float radius) { m_radius = radius; }
+
 	float GetRadius() const { return m_radius; }
+
 private:
 	float m_radius;
 };
-
-// create one for planes
-// create one of these for AABB
-// instead of radius - extends, direction or distance for type of object.
