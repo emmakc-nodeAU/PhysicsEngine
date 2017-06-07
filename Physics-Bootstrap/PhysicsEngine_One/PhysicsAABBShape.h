63832ae2506814acc87872cf0587ebd247079de4
Emma Cameron <emmakc@gmail.com>
2017-06-07 12:23:34 +1000

    PhysicsAABBShape commetns cleanup of height/width, only using extents

--- Physics-Bootstrap/PhysicsEngine_One/PhysicsAABBShape.h
+++ Physics-Bootstrap/PhysicsEngine_One/PhysicsAABBShape.h
@@ -13,19 +13,9 @@ public:
 	{
 
 	}
-
 	void SetExtents(glm::vec3 extents) { m_extents = extents; }
 	glm::vec3 GetExtents() const { return m_extents; }
 
-//	void SetWidth(glm::vec3 width) { m_width = width; }
-//	float GetWidth() const { return m_width; }
-//
-//	void SetHeight(glm::vec3 height) { m_height = height; }
-//	float GetHeight() const { return m_height; }
-
 private:
 	glm::vec3 m_extents;
-	//glm::vec3 m_width;
-	//glm::vec3 m_height;
-	//glm::vec3 pShape;
 };
